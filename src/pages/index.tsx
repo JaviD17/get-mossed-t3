686ee4b418375ef3b7b157e3f6cac795e6140f7d
Javier Dominguez II <Javi7057@gmail.com>
2023-04-17 20:59:58 -0500

    finished adding posts Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import { api } from "~/utils/api";
 const Home: NextPage = () => {
   const user = useUser();
 
-  const { data } = api.example.getAll.useQuery();
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -22,7 +22,11 @@ const Home: NextPage = () => {
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
