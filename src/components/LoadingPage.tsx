2262785dd70b1529db753ae4cb0fcef5ed1ce83e
Javier Dominguez II <Javi7057@gmail.com>
2023-04-18 19:57:59 -0500

    updated title in head, and created Feed componenet and seperate loading states

---
+++ src/components/LoadingPage.tsx
@@ -0,0 +1,11 @@
+import Loading from "./Loading";
+
+const LoadingPage = () => {
+  return (
+    <div className="absolute right-0 top-0 flex h-screen w-screen items-center justify-center">
+      <Loading />
+    </div>
+  );
+};
+
+export default LoadingPage;
