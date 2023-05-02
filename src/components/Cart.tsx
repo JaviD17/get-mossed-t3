5135d7daffc9a8bda5749acbe6bcd8498b572d1b
Javier Dominguez II <Javi7057@gmail.com>
2023-05-02 18:21:32 -0500

    added optional props to Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,4 @@
 import { motion } from "framer-motion";
-import Image from "next/image";
-import Link from "next/link";
 
 const Cart = (props: { firstName: string }) => {
   return (
@@ -12,7 +10,7 @@ const Cart = (props: { firstName: string }) => {
       className="absolute flex w-full flex-col bg-slate-700 md:left-1/2 md:w-1/2"
     >
       <h3 className="py-3 text-center text-2xl font-thin tracking-widest">
-        {props.firstName}'s Cart
+        {props?.firstName}'s Cart
       </h3>
     </motion.aside>
   );
