a5410af87887f9c0f254330c14190e05687e8acc
Javier Dominguez II <Javi7057@gmail.com>
2023-05-02 18:23:28 -0500

    added {} for accidental escaped '

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -10,7 +10,8 @@ const Cart = (props: { firstName: string }) => {
       className="absolute flex w-full flex-col bg-slate-700 md:left-1/2 md:w-1/2"
     >
       <h3 className="py-3 text-center text-2xl font-thin tracking-widest">
-        {props?.firstName}'s Cart
+        {props?.firstName}
+        {"'s Cart"}
       </h3>
     </motion.aside>
   );
